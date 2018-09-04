155a2e0efd897d35e6c9a0066f5c3393753b6294
Graham Goldstein <g.goldstein@live.com>
2018-09-04 20:40:51 +0100

    Add ConvexPolygon and tests

---
+++ FortunesAlgorithm/ConvexPolygon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FortunesAlgorithm
+{
+    public class ConvexPolygon
+    {
+        Point centroid;
+        Dictionary<Point, Point> next;
+        Dictionary<Point, Point> previous;
+
+        public ConvexPolygon(IEnumerable<Point> points)
+        {
+            centroid = Centroid(points);
+            List<Point> orderedPoints = OrderPoints(centroid, points);
+            int count = orderedPoints.Count;
+            next = new Dictionary<Point, Point>();
+            previous = new Dictionary<Point, Point>();
+            for (int i = 0; i < count; i++ )
+            {
+                if (i + 1 < count)
+                    next[orderedPoints[i]] = orderedPoints[i + 1];
+                else
+                    next[orderedPoints[i]] = orderedPoints[0];
+                if (i > 0)
+                    previous[orderedPoints[i]] = orderedPoints[i - 1];
+                else
+                    previous[orderedPoints[i]] = orderedPoints[count - 1];
+            }
+        }
+
+        public Point NextVertex(Point point)
+        {
+            return next[point];
+        }
+
+        public Point PreviousVertex(Point point)
+        {
+            return previous[point];
+        }
+
+        public Point Centroid(IEnumerable<Point> points)
+        {
+            float xSum = points.Aggregate(0f, (total, next) => total + next.Cartesianx());
+            float ySum = points.Aggregate(0f, (total, next) => total + next.Cartesiany());
+            float xMean = xSum / points.Count();
+            float yMean = ySum / points.Count();
+            return new Point(xMean, yMean);
+        }
+
+        public List<Point> OrderPoints(Point centroid, IEnumerable<Point> points)
+        {
+            return points.OrderBy((point) => AngleFromOrigin(centroid, point)).ToList();
+        }
+
+        public float AngleFromOrigin(Point origin, Point point)
+        {
+            return (float)Math.Atan2(point.Cartesiany() - origin.Cartesiany(), point.Cartesianx() - origin.Cartesianx());
+        }
+    }
+}
---
+++ FortunesAlgorithmTest/Geometry/ConvexPolygonTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using FortunesAlgorithm;
+using System.Collections.Generic;
+
+namespace Geometry
+{
+    [TestFixture]
+    public class ConvexSquare
+    {
+        List<Point> vertices;
+        ConvexPolygon poly;
+        static Point topLeft;
+        static Point topRight;
+        static Point bottomLeft;
+        static Point bottomRight;
+
+        [SetUp]
+        public void SetUp()
+        { 
+            SetPoints();
+            vertices = new List<Point> { topLeft, bottomRight, bottomLeft, topRight };
+            poly = new ConvexPolygon(vertices);
+        }
+
+        static void SetPoints()
+        {
+            topLeft = new Point(-1, 1);
+            topRight = new Point(1, 1);
+            bottomLeft = new Point(-1, -1);
+            bottomRight = new Point(1, -1);
+        }
+
+        [TestCaseSource("NextPointsData")]
+        public void NextPoints(Point a, Point b)
+        {
+            Assert.AreEqual(b, poly.NextVertex(a));
+        }
+
+        [TestCaseSource("PreviousPointsData")]
+        public void PreviousPoints(Point a, Point b)
+        {
+            Assert.AreEqual(b, poly.PreviousVertex(a));
+        }
+
+        public static IEnumerable<ITestCaseData> NextPointsData
+        {
+            get
+            {
+                SetPoints();
+                yield return new TestCaseData(topRight, topLeft);
+                yield return new TestCaseData(topLeft, bottomLeft);
+                yield return new TestCaseData(bottomLeft, bottomRight);
+                yield return new TestCaseData(bottomRight, topRight);
+            }
+        }
+
+        public static IEnumerable<ITestCaseData> PreviousPointsData
+        {
+            get
+            {
+                SetPoints();
+                yield return new TestCaseData(topRight, bottomRight);
+                yield return new TestCaseData(topLeft, topRight);
+                yield return new TestCaseData(bottomLeft, topLeft);
+                yield return new TestCaseData(bottomRight, bottomLeft);
+            }
+        }
+    }
+}
